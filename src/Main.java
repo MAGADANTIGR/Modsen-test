3e346440f9bb6d28685072fb2aa94c3a9a7f596f
Vova <073601@gmail.com>
2023-06-12 22:37:01 +0300

    commit3

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,84 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Выберите пункт\n1. Перевод валюты ($->RUB)\n2. Перевод валюты (RUB->$)\n3. Сложение\n");
-        int v = scanner.nextInt();
         Converter converter = new Converter();
-        switch (v){
+        int ch1, ch2;
+        String str = "";
+        System.out.println("Выберите пункт\n1. Перевод валюты ($->RUB)\n2. Перевод валюты (RUB->$)\n3. Сложение\n4. Вычитание");
+        switch (scanner.nextInt()){
             case 1: System.out.println("Введите сумму в $");
-            System.out.println(converter.toRubles(scanner.nextInt()));
+            str = String.format("%.2f",converter.toRubles(scanner.nextDouble()));
+            System.out.print(str + "p");
             break;
 
             case 2: System.out.println("Введите сумму в RUB");
-            System.out.println(converter.toDollars(scanner.nextInt()));
+            str = String.format("%.2f", converter.toDollars(scanner.nextDouble()));
+            System.out.print("$" + str);
             break;
 
+            case 3: System.out.println("Выберите, что нужно сложить\n1. $ + $\n2. RUB + RUB\n3. $ + RUB");
+            switch (scanner.nextInt()){
+                case 1: System.out.println("Введите суммы");
+                str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), '$', '+', '-', 0));
+                System.out.print("$"+str);
+                break;
+
+                case 2: System.out.println("Введите суммы");
+                str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), 'R', '+', '-', 0));
+                System.out.print(str+"p");
+                break;
+
+                case 3: System.out.println("Выберите валюту, в которой нужно произвести расчет\n1. $\n2. RUB");
+                if(scanner.nextInt()==1){
+                    System.out.println("Введите суммы (первая сумма-$, вторая-RUB)");
+                    str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), 'S', '+', '$', 0));
+                    System.out.print("$"+str);
+                } else {
+                    System.out.println("Введите суммы (первая сумма-$, вторая-RUB)");
+                    str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), 'S', '+', 'R', 0));
+                    System.out.print(str+"p");
+                }
+                break;
+            }
+            break;
+
+            case 4: System.out.println("Выберите, что нужно вычесть\n1. $ - $\n2. RUB - RUB\n3. $ - RUB\n4. RUB - $");
+                switch (scanner.nextInt()){
+                    case 1: System.out.println("Введите суммы");
+                    str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), '$', '-', '-', 0));
+                    System.out.print("$"+str);
+                    break;
+
+                    case 2: System.out.println("Введите суммы");
+                    str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), 'R', '-', '-', 0));
+                    System.out.print(str+"p");
+                    break;
+
+                    case 3: System.out.println("Выберите валюту, в которой нужно произвести расчет\n1. $\n2. RUB");
+                        if(scanner.nextInt()==1){
+                            System.out.println("Введите суммы (первая сумма-$, вторая-RUB)");
+                            str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), 'S', '-', '$', 1));
+                            System.out.print("$"+str);
+                        } else {
+                            System.out.println("Введите суммы (первая сумма-$, вторая-RUB)");
+                            str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), 'S', '-', 'R', 1));
+                            System.out.print(str+"p");
+                        }
+                        break;
+
+                    case 4: System.out.println("Выберите валюту, в которой нужно произвести расчет\n1. $\n2. RUB");
+                        if(scanner.nextInt()==1){
+                            System.out.println("Введите суммы (первая сумма-RUB, вторая-$)");
+                            str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), 'S', '-', '$', 2));
+                            System.out.print("$"+str);
+                        } else {
+                            System.out.println("Введите суммы (первая сумма-RUB, вторая-$)");
+                            str = String.format("%.2f", converter.operations(scanner.nextDouble(), scanner.nextDouble(), 'S', '-', 'R', 2));
+                            System.out.print(str+"p");
+                        }
+                        break;
+                }
+                break;
             default: break;
         }
     }
