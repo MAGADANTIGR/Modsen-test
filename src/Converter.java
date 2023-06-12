e3a7d75987d5302ccaa08a0d9e6d3f385cf0221d
Vova <073601@gmail.com>
2023-06-12 19:42:31 +0300

    commit2

---
+++ src/Converter.java
@@ -0,0 +1,22 @@
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
+public class Converter {
+    public Converter() throws IOException, ParseException {
+    }
+    Object o = new JSONParser().parse(new FileReader("file.json"));
+    JSONObject j = (JSONObject) o;
+
+    private double cRubles = Double.parseDouble((String) j.get("toRubles"));
+    private double cDollars = Double.parseDouble((String)j.get("toDollars"));
+
+    public double toRubles(double dollars) {
+        return dollars*cRubles;
+    }
+
+    public double toDollars(double rubles) {
+        return rubles*cDollars;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,24 @@
+import org.json.simple.parser.ParseException;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) throws IOException, ParseException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите пункт\n1. Перевод валюты ($->RUB)\n2. Перевод валюты (RUB->$)\n3. Сложение\n");
+        int v = scanner.nextInt();
+        Converter converter = new Converter();
+        switch (v){
+            case 1: System.out.println("Введите сумму в $");
+            System.out.println(converter.toRubles(scanner.nextInt()));
+            break;
+
+            case 2: System.out.println("Введите сумму в RUB");
+            System.out.println(converter.toDollars(scanner.nextInt()));
+            break;
+
+            default: break;
+        }
+    }
 }
