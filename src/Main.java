eb51d819a95f1189f651e04804be02e298cda5bc
Vova <073601@gmail.com>
2023-06-28 02:02:21 +0300

    переделала

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,16 @@ public class Main {
             System.out.printf("Результат сложения: %.2fр\n", Double.parseDouble(str.substring(0, str.indexOf("р")))+Double.parseDouble(str.substring(str.indexOf("+")+1, str.lastIndexOf("р"))));
         } else if(str.matches("^(\\$)\\d+(\\.)?\\d*(\\+)(\\$)\\d+(\\.)?\\d*$")){
             System.out.printf("Результат сложения: $%.2f\n", Double.parseDouble(str.substring(1, str.indexOf("+")))+Double.parseDouble(str.substring(str.lastIndexOf("$")+1)));
+        } else if(str.matches("^(\\$)\\d+(\\.)?\\d*-(\\$)\\d+(\\.)?\\d*$")){
+            System.out.printf("Результат вычитания: $%.2f\n", Double.parseDouble(str.substring(1, str.indexOf("-")))-Double.parseDouble(str.substring(str.lastIndexOf("$")+1)));
+        } else if(str.matches("^\\d+(\\.)?\\d*р-\\d+(\\.)?\\d*р$")){
+            System.out.printf("Результат вычитания: %.2fр\n", Double.parseDouble(str.substring(0, str.indexOf("р")))-Double.parseDouble(str.substring(str.indexOf("-")+1, str.lastIndexOf("р"))));
+        } else if(str.matches("^(\\$)\\d+(\\.)?\\d*-\\d+(\\.)?\\d*р$")){
+            System.out.printf("Результат вычитания в рублях: %.2fр\n", converter.toRubles(Double.parseDouble(str.substring(1, str.indexOf("-"))))-Double.parseDouble(str.substring(str.indexOf("-")+1, str.indexOf("р"))));
+            System.out.printf("Результат вычитания в долларах: $%.2f\n", Double.parseDouble(str.substring(1, str.indexOf("-")))-converter.toDollars(Double.parseDouble(str.substring(str.indexOf("-")+1, str.indexOf("р")))));
+        } else if(str.matches("^\\d+(\\.)?\\d*р-(\\$)\\d+(\\.)?\\d*$")){
+            System.out.printf("Результат вычитания в рублях: %.2fр\n", Double.parseDouble(str.substring(0, str.indexOf("р")))-converter.toRubles(Double.parseDouble(str.substring(str.indexOf("$")+1))));
+            System.out.printf("Результат вычитания в долларах: $%.2f\n", converter.toDollars(Double.parseDouble(str.substring(0, str.indexOf("р"))))-Double.parseDouble(str.substring(str.indexOf("$")+1)));
         } else {
             System.out.println("Неверный ввод данных!");
         }
